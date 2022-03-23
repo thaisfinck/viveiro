e9eba6fddb2a346d8043e7cd0dfe6847d9a296d0
thaisfinck <thaisfinck@gmail.com>
2022-03-23 15:46:47 +0100

    feat(navbar): grid and links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,44 @@
 import React from 'react'
 
+
 export default function Navbar() {
   return (
     <div>
-        <h1 className='title'>Viveiro Agriflorestal | O viveiro | Quem Somos | O que produzimos | Contato</h1>
+      <div class="container">
+        <div class="row">
+          <div class="col-5 content">
+          <img src="viveiro.png" class="img-fluid mt-3" width="200px" alt="logo viveiro agriflorestal"></img>
+          </div>
+          <div class="col content pt-4">
+            <a href='./oviveiro'>
+              <button>
+              O viveiro
+              </button>
+            </a>
+          </div>
+          <div class="col content pt-4">
+            <a href='./quemsomos'>
+              <button>
+            Quem Somos
+              </button>
+            </a>
+          </div>
+          <div class="col-2 content pt-4">
+            <a href='./oqueproduzimos'>
+              <button>
+            O Que Produzimos
+              </button>
+            </a>
+          </div>
+          <div class="col content pt-4">
+            <a href='./contato'>
+              <button>
+            Contato
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
