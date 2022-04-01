ced2fa5bb8916303c00df7d17da5f3bb0eefc774
thaisfinck <thaisfinck@gmail.com>
2022-04-01 11:37:04 +0200

    feat(navbar): styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,44 +3,29 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className="sticky-top bg-white">
-      <nav>
-      <div className="container">
-        <div className="row">
-          <div className="col-5 content">
-          <img src="logo3.png" className="img-fluid mt-3" width="200px" alt="logo viveiro agriflorestal" id='logo'></img>
-          </div>
-          <div className="col content pt-5">
-            <a href='#oviveiro'>
-              <button className="bg-transparent border-0">
-              O Viveiro
-              </button>
-            </a>
-          </div>
-          <div className="col content pt-5">
-            <a href='#quemsomos'>
-              <button className="bg-transparent border-0" >
-            Quem Somos
-              </button>
-            </a>
-          </div>
-          <div className="col-2 content pt-5">
-            <a href='#oqueproduzimos'>
-              <button className="bg-transparent border-0">
-            O Que Produzimos
-              </button>
-            </a>
-          </div>
-          <div className="col content pt-5">
-            <a href='#contato'>
-              <button className="bg-transparent border-0">
-            Contato
-              </button>
-            </a>
-          </div>
-        </div>
+  <div className="sticky-top bg-white">
+    <nav className="navbar navbar-expand-lg navbar-light bg-white">
+      <img src="ico.png" className="img-fluid" width="80px" alt="logo viveiro agriflorestal" id='logo'></img>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse justify-content-md-end" id="navbarsExample10">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="p-4 text-dark navlink" href="#oviveiro">O VIVEIRO</a>
+          </li>
+          <li className="nav-item">
+            <a className="p-4 text-dark navlink" href="#quemsomos">QUEM SOMOS</a>
+          </li>
+          <li className="nav-item">
+            <a className="p-4 text-dark navlink" href="#oqueproduzimos">O QUE PRODUZIMOS</a>
+          </li>
+          <li className="nav-item">
+            <a className="p-4 text-dark navlink" href="#contato">CONTATO</a>
+          </li>
+        </ul>
       </div>
-      </nav>
-    </div>
+    </nav>
+  </div>
   )
 }
