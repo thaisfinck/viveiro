c8acecef9b81efbcba7855b77b7254de374b4c2d
thaisfinck <thaisfinck@gmail.com>
2022-04-06 17:55:31 +0200

    feat(oqueproduzimos): card style change

--- src/components/oqueproduzimos.js
+++ src/components/oqueproduzimos.js
@@ -3,47 +3,105 @@ import React from 'react'
 export default function Oqueproduzimos() {
   return (
     <section id='oqueproduzimos' className="container">
+      <div id="oqueproduzimos-text">
       <h1 className='title text-center mb-3'><strong>O QUE PRODUZIMOS</strong></h1>
-      <p className='text-justify content'>O Viveiro Agriflorestal fica localizado na cidade de Urubici e
+      <p className='text-center content'>O Viveiro Agriflorestal fica localizado na cidade de Urubici e
           vem fornecendo mudas de diversos tipos para a toda a comunidade urubiciense desde 1990. 
           Nosso objetivo é fornecer um servico de qualidade para nossos clientes e 
           nosso maior retorno e ver o agricultor ter bons resultados.
         </p>
-         <div className="container mt-4 mb-4">
+      </div>
+        <div className="container mt-4 mb-4" id="cards">
           <div className="row">
             <div className="col">
-            <div className="card" style={{width: 250, height: 330}}>
-                <img src="lettuce.jpeg" className="card-img-top" alt="alface" style={{width: 248, height: 150}}></img>
-                <div className="card-body">
-                  <h5 className="card-title content">Alface</h5>
-                  <p className="card-text content">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className="card bg-transparent border-0" style={{width: 250}}>
+              <div className="card-body">
+                <h5 className="card-title title"><strong>ALFACE</strong></h5>
+                  <h6 className="card-subtitle mb-2 text-muted content">Tipos</h6>
+                  <hr />
+                    <ul className="card-text content">
+                      <li>Alface Verde</li>
+                      <li>Alface Roxa</li>
+                      <li>Alface Vermelha</li>
+                      <li>Alface Amarela</li>
+                    </ul>
                 </div>
               </div>
             </div>
             <div className="col">
-            <div className="card" style={{width: 250, height: 330}}>
-                <img src="lettuce.jpeg" className="card-img-top" alt="alface" style={{width: 248, height: 150}}></img>
-                <div className="card-body">
-                  <h5 className="card-title content">Cebola</h5>
-                  <p className="card-text content">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className="card bg-transparent border-0" style={{width: 250}}>
+              <div className="card-body">
+                <h5 className="card-title title"><strong>CEBOLA</strong></h5>
+                  <h6 className="card-subtitle mb-2 text-muted content">Tipos</h6>
+                  <hr />
+                  <ul className="card-text">
+                      <li>Cebola Verde</li>
+                      <li>Cebola Roxa</li>
+                      <li>Cebola Vermelha</li>
+                      <li>Cebola Amarela</li>
+                    </ul>                
                 </div>
               </div>
             </div>
             <div className="col">
-            <div className="card" style={{width: 250, height: 330}}>
-                <img src="lettuce.jpeg" className="card-img-top" alt="alface" style={{width: 248, height: 150}}></img>
-                <div className="card-body">
-                  <h5 className="card-title content">Cenoura</h5>
-                  <p className="card-text content">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className="card bg-transparent border-0" style={{width: 250}}>
+              <div className="card-body">
+                <h5 className="card-title title"><strong>TOMATE</strong></h5>
+                  <h6 className="card-subtitle mb-2 text-muted content">Tipos</h6>
+                  <hr />
+                  <ul className="card-text">
+                      <li>Tomate Verde</li>
+                      <li>Tomate Roxa</li>
+                      <li>Tomate Vermelha</li>
+                      <li>Tomate Amarela</li>
+                    </ul>                
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row mt-3">
+            <div className="col">
+            <div className="card bg-transparent border-0" style={{width: 250}}>
+              <div className="card-body">
+                <h5 className="card-title title"><strong>ALFACE</strong></h5>
+                  <h6 className="card-subtitle mb-2 text-muted content">Tipos</h6>
+                  <hr />
+                    <ul className="card-text content">
+                      <li>Alface Verde</li>
+                      <li>Alface Roxa</li>
+                      <li>Alface Vermelha</li>
+                      <li>Alface Amarela</li>
+                    </ul>
                 </div>
               </div>
             </div>
             <div className="col">
-            <div className="card" style={{width: 250, height: 330}}>
-                <img src="lettuce.jpeg" className="card-img-top" alt="alface" style={{width: 248, height: 150}}></img>
-                <div className="card-body">
-                  <h5 className="card-title content">Couve</h5>
-                  <p className="card-text content">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className="card bg-transparent border-0" style={{width: 250}}>
+              <div className="card-body">
+                <h5 className="card-title title"><strong>CEBOLA</strong></h5>
+                  <h6 className="card-subtitle mb-2 text-muted content">Tipos</h6>
+                  <hr />
+                  <ul className="card-text">
+                      <li>Cebola Verde</li>
+                      <li>Cebola Roxa</li>
+                      <li>Cebola Vermelha</li>
+                      <li>Cebola Amarela</li>
+                    </ul>                
+                </div>
+              </div>
+            </div>
+            <div className="col">
+            <div className="card bg-transparent border-0" style={{width: 250}}>
+              <div className="card-body">
+                <h5 className="card-title title"><strong>TOMATE</strong></h5>
+                  <h6 className="card-subtitle mb-2 text-muted content">Tipos</h6>
+                  <hr />
+                  <ul className="card-text">
+                      <li>Tomate Verde</li>
+                      <li>Tomate Roxa</li>
+                      <li>Tomate Vermelha</li>
+                      <li>Tomate Amarela</li>
+                    </ul>                
                 </div>
               </div>
             </div>
