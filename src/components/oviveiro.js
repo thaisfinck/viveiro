3553392cf449ca4a0fff5b9b62f146e8ca0e2a33
thaisfinck <thaisfinck@gmail.com>
2022-04-06 17:55:59 +0200

    feat(oviveiro): media query not finished

--- src/components/oviveiro.js
+++ src/components/oviveiro.js
@@ -7,45 +7,48 @@ export default function Oviveiro() {
       <h3 className='text-center text-uppercase pt-5' id='subtitle'>VIVEIRO DE MUDAS</h3>
         <h1 className='text-center text-uppercase mb-3' id='titlemain'><strong>AGRIFLORESTAL</strong></h1>
       </div>
-      <div className="container grid">
-          <div className="row ms-5" style={{height: 200}}>
-            <div className="col">
-              <div className="card position-relative bottom-50" style={{width: 250, height: 350}}>
-                <div className="card-body">
-                  <h5 className="card-title content">Servico de Qualidade</h5>
+      <div className="container" id="blocks">
+          <div className="row">
+            <div className="col-4 main-blocks">
+              <div className="card position-relative bottom-50 main-card">
+                <div className="card-body main-body-card">
+                  <h5 className="card-title content">Mão de Obra Qualificada</h5>
                   <p className="card-text content">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
-                <div className='card-icon text-center pt-5'><i className="fas fa-seedling fa-5x"></i></div>
+                <div className='card-icon text-center pt-5'><i className="fas fa-seedling"></i></div>
               </div>
             </div>
-            <div className="col">
-              <div className="card position-relative bottom-50" style={{width: 250, height: 350}}>
-                <div className="card-body">
+            <div className="col-4 main-blocks">
+              <div className="card position-relative bottom-50 main-card" >
+                <div className="card-body main-body-card">
                   <h5 className="card-title content">Profissionalismo</h5>
                   <p className="card-text content">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
-                <div className='card-icon text-center pt-5'><i className="fas fa-hands-helping fa-5x"></i></div>
+                <div className='card-icon text-center pt-5'><i className="fas fa-hands-helping"></i></div>
              </div>
             </div>
-            <div className="col">
-              <div className="card position-relative bottom-50" style={{width: 250, height: 350}}>
-                <div className="card-body">
-                  <h5 className="card-title content">Entrega</h5>
+            <div className="col-4 main-blocks" id="extra">
+              <div className="card position-relative bottom-50 main-card">
+                <div className="card-body main-body-card">
+                  <h5 className="card-title content">Entrega e Controle</h5>
                   <p className="card-text content">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
-                <div className='card-icon text-center pt-5'><i className="fas fa-truck fa-5x"></i></div>
+                <div className='card-icon text-center pt-5'><i className="fas fa-truck"></i></div>
               </div>
             </div>
           </div>
       </div>
-      <div className='p-5' id="oviveiro-text">
+      <div id="oviveiro-text">
       <h1 className='title text-center mb-3'><strong>O VIVEIRO</strong></h1>
-          <div className="content align-middle mb-4">
+          <div className="content text-center mb-4">
             <p>O Viveiro Agriflorestal fica localizado na cidade de Urubici e
           vem fornecendo mudas de diversos tipos para a toda a comunidade urubiciense desde 1990. 
           Nosso objetivo é fornecer um servico de qualidade para nossos clientes e 
           nosso maior retorno e ver o agricultor ter bons resultados.</p>
           </div>
+        </div>
+        <div id="video" class="container ratio ratio-16x9 mb-5">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/3wG4ngW7k7w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     </section>
   )
--- src/components/quemsomos.js
+++ src/components/quemsomos.js
@@ -9,6 +9,44 @@ export default function Quemsomos() {
           Nosso objetivo é fornecer um servico de qualidade para nossos clientes e 
           nosso maior retorno e ver o agricultor ter bons resultados.
         </p>
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" >
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp"
+                class="d-block w-100"
+                alt="Sunset Over the City"
+              />
+            </div>
+            <div class="carousel-item">
+            <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp"
+                class="d-block w-100"
+                alt="Canyon at Nigh"
+              />
+            </div>
+            <div class="carousel-item">
+            <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp"
+                class="d-block w-100"
+                alt="Cliff Above a Stormy Sea"
+              />
+            </div>
+          </div>
+          <button class="carousel-control-prev" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
     </section>
   )
 }
