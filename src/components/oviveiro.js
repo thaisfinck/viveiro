6696911e465ae767712badb47fd58246974191d5
thaisfinck <thaisfinck@gmail.com>
2022-04-07 15:39:13 +0200

    feat(oviveiro) : mediaquery finished

--- src/components/oviveiro.js
+++ src/components/oviveiro.js
@@ -7,37 +7,124 @@ export default function Oviveiro() {
       <h3 className='text-center text-uppercase pt-5' id='subtitle'>VIVEIRO DE MUDAS</h3>
         <h1 className='text-center text-uppercase mb-3' id='titlemain'><strong>AGRIFLORESTAL</strong></h1>
       </div>
-      <div className="container" id="blocks">
-          <div className="row">
-            <div className="col-4 main-blocks">
-              <div className="card position-relative bottom-50 main-card">
-                <div className="card-body main-body-card">
-                  <h5 className="card-title content">Mão de Obra Qualificada</h5>
-                  <p className="card-text content">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+      <div
+        id="carouselMultiItemExample"
+        className="carousel slide carousel-dark text-center"
+        data-mdb-ride="carousel"
+      >
+        <div className="d-flex justify-content-center mb-4">
+          <button
+            className="carousel-control-prev position-relative"
+            type="button"
+            data-mdb-target="#carouselMultiItemExample"
+            data-mdb-slide="prev"
+          >
+            <span><i className="fas fa-chevron-left left-right"></i></span>
+          </button>
+          <button
+            className="carousel-control-next position-relative light"
+            type="button"
+            data-mdb-target="#carouselMultiItemExample"
+            data-mdb-slide="next"
+          >
+            <span><i className="fas fa-chevron-right left-right"></i></span>
+          </button>
+        </div>
+        <div className="carousel-inner py-4">
+          <div className="carousel-item active">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-4">
+                  <div className="card">
+                  <div className="card-body">
+                      <h5 className="card-title">Mão de Obra de Qualidade</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and make up the bulk
+                        of the card's content.
+                      </p>
+                    </div>
+                    <div className='card-icon text-center p-3'><i className="fas fa-seedling main-icon"></i></div>
+                  </div>
                 </div>
-                <div className='card-icon text-center pt-5'><i className="fas fa-seedling"></i></div>
-              </div>
-            </div>
-            <div className="col-4 main-blocks">
-              <div className="card position-relative bottom-50 main-card" >
-                <div className="card-body main-body-card">
-                  <h5 className="card-title content">Profissionalismo</h5>
-                  <p className="card-text content">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                <div className="col-lg-4 d-none d-lg-block">
+                  <div className="card">
+                  <div className="card-body">
+                      <h5 className="card-title">Profissionalismo</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and make up the bulk
+                        of the card's content.
+                      </p>
+                    </div>
+                    <div className='card-icon text-center p-3'><i className="fas fa-hands-helping main-icon"></i></div>
+                  </div>
                 </div>
-                <div className='card-icon text-center pt-5'><i className="fas fa-hands-helping"></i></div>
-             </div>
-            </div>
-            <div className="col-4 main-blocks" id="extra">
-              <div className="card position-relative bottom-50 main-card">
-                <div className="card-body main-body-card">
-                  <h5 className="card-title content">Entrega e Controle</h5>
-                  <p className="card-text content">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                <div className="col-lg-4 d-none d-lg-block">
+                  <div className="card">
+                  <div className="card-body">
+                      <h5 className="card-title">Entrega</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and make up the bulk
+                        of the card's content.
+                      </p>
+                    </div>
+                    <div className='card-icon text-center p-3'><i className="fas fa-truck main-icon"></i></div>
+                  </div>
                 </div>
-                <div className='card-icon text-center pt-5'><i className="fas fa-truck"></i></div>
               </div>
             </div>
           </div>
+
+          <div className="carousel-item">
+          <div className="container">
+              <div className="row">
+                <div className="col-lg-4">
+                  <div className="card">
+                  <div className="card-body">
+                      <h5 className="card-title">Mão de Obra de Qualidade</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and make up the bulk
+                        of the card's content.
+                      </p>
+                    </div>
+                    <div className='card-icon text-center p-3'><i className="fas fa-seedling main-icon"></i></div>
+                  </div>
+                </div>
+
+                <div className="col-lg-4 d-none d-lg-block">
+                  <div className="card">
+                  <div className="card-body">
+                      <h5 className="card-title">Profissionalismo</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and make up the bulk
+                        of the card's content.
+                      </p>
+                    </div>
+                    <div className='card-icon text-center p-3'><i className="fas fa-hands-helping main-icon"></i></div>
+                  </div>
+                </div>
+
+                <div className="col-lg-4 d-none d-lg-block">
+                  <div className="card">
+                  <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and make up the bulk
+                        of the card's content.
+                      </p>
+                    </div>
+                    <div className='card-icon text-center p-3'><i className="fas fa-truck main-icon"></i></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      <div id="main-content" className='pb-3 pt-5'>
+      <p className="content text-center"><i class="fas fa-ellipsis-h" id="separator-one"></i></p>
       <div id="oviveiro-text">
       <h1 className='title text-center mb-3'><strong>O VIVEIRO</strong></h1>
           <div className="content text-center mb-4">
@@ -47,9 +134,11 @@ export default function Oviveiro() {
           nosso maior retorno e ver o agricultor ter bons resultados.</p>
           </div>
         </div>
-        <div id="video" class="container ratio ratio-16x9 mb-5">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/3wG4ngW7k7w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div id="video" className="container ratio ratio-16x9 mb-5">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/3wG4ngW7k7w" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
+        <p className="content text-center"><i class="fas fa-ellipsis-h" id="separator-two"></i></p>
+      </div>
     </section>
   )
 }
