c0cbfe76aff7d3e45c0624c7a8df0f04c51eebdd
thaisfinck <thaisfinck@gmail.com>
2022-05-04 18:52:05 +0200

    feat(navbar): no 'oqueproduzimos' no more

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
   <div className="sticky-top bg-white">
     <nav className="navbar navbar-expand-lg navbar-light bg-white">
-      <img src="ico.png" className="img-fluid" width="80px" alt="logo viveiro agriflorestal" id='logo'></img>
+      <a href="#headline"><img src="ico.png" className="img-fluid" width="80px" alt="logo viveiro agriflorestal" id='logo'></img></a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -13,9 +13,6 @@ export default function Navbar() {
         <ul className="navbar-nav">
           <li className="nav-item">
             <a className="p-4 text-dark navlink" href="#oviveiro">O VIVEIRO</a>
-          </li>
-          <li className="nav-item">
-            <a className="p-4 text-dark navlink" href="#oqueproduzimos">O QUE PRODUZIMOS</a>
           </li>
           <li className="nav-item">
             <a className="p-4 text-dark navlink" href="#quemsomos">QUEM SOMOS</a>
